965539b6efc308c35a67c673c1572809b526b430
Cesar <c_sanchez21@hotmail.com>
2024-05-07 13:41:03 -0700

    Added TreeView Test

---
+++ TreeView/TreeView/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Note: ObservableCollection must be a public property {i.e have get; set;} to enable binding. 
+        public ObservableCollection<Node> Nodes { get; set; } = new ObservableCollection<Node>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            InitializeNodes();
+        }
+
+        private void InitializeNodes()
+        {
+            //Nodes = new ObservableCollection<Node>();
+            Node root = new Node() { Name = "Root" };
+            Node child1 = new Node() { Name = "Child A" };
+            child1.SubNodes.Add(new Node() { Name = "Grandchild A1" });
+            child1.SubNodes.Add(new Node() { Name = "Grandchild A2" });
+            Node child2 = new Node() { Name = "Child B" };
+            child2.SubNodes.Add(new Node() { Name = "Grandchild B1" });
+            child2.SubNodes.Add(new Node() { Name = "Grandchild B2" });
+            root.SubNodes.Add(child1);
+            root.SubNodes.Add(child2);
+            Nodes.Add(root);
+        }
+        
+        private void btnAddChildren_Click(object sender, RoutedEventArgs e)
+        {
+            if (Nodes == null) return;
+
+            if (Nodes.Count > 0)
+            {
+                Node sub = Nodes[Nodes.Count - 1];
+                sub.SubNodes.Add(new Node() { Name = $"Child {sub.SubNodes.Count + 1}" });
+            }
+                                                        
+            Nodes.Add(new Node() { Name= $"Root {Nodes.Count+1}"});
+        }
+    }
+}
---
+++ TreeView/TreeView/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeView
+{
+    public class Node
+    {
+        public string Name { get; set; }
+
+        //NOTE: SubNodes must be an ObservableCollection to be able to display real time changes.
+        public ObservableCollection<Node> SubNodes { get; set; } = new ObservableCollection<Node>();
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
