3c2fb979a7469786d1382a962fd712a486efbff0
Cesar <c_sanchez21@hotmail.com>
2024-05-11 15:09:14 -0700

    Added Bitmap Testing

---
+++ Bitmaps/Bitmaps/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Bitmaps
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Bitmaps/Bitmaps/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bitmaps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_OpenClick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            bool? result = openFileDialog.ShowDialog();
+            if(result == true)
+            {
+                string fullpath = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(fullpath);
+                bitmap.EndInit();
+                imgViewer.Source = bitmap;
+                ShowBitmapArray(bitmap);
+            }
+        }
+
+        private void ShowBitmapArray(BitmapImage bmp)
+        {
+            int stride = (int)bmp.PixelWidth * (bmp.Format.BitsPerPixel / 8);
+            byte[] pixels = new byte[(int)bmp.PixelHeight * stride];
+            bmp.CopyPixels(pixels, stride, 0);
+            string dataString = String.Join(" ", pixels);
+            //glyphImageData.UnicodeString = dataString; //Crashes
+            //lblImgData.Content = dataString; //No Wrapping
+            //tbImageData.Text = dataString; //Very Slow
+            rtbImageData.Document.Blocks.Clear();
+            rtbImageData.Document.Blocks.Add(new Paragraph(new Run(dataString)));
+        }
+    }
+}
