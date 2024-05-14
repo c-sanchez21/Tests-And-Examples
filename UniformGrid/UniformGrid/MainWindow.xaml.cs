d02173ba30c2adf5517f941ef1b23e6099f1f292
Cesar <c_sanchez21@hotmail.com>
2024-05-13 20:25:36 -0700

    Added Uniform Grid Test

---
+++ UniformGrid/UniformGrid/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UniformGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitGrid();
+        }
+
+        private void InitGrid()
+        {
+            DateTime date = DateTime.Today;
+            for(int i = 1; i <=  DateTime.DaysInMonth(date.Year, date.Month); i++)
+            {
+                uniGrid.Children.Add(new Label() { Content = i.ToString() });
+            }
+
+        }
+    }
+}
