9a08a0fafa3ae9f68e51b5a1cc31f301ae47b10c
Cesar <c_sanchez21@hotmail.com>
2024-05-22 21:44:06 -0700

    Added Gradient Background and Selecteable Days

--- UniformGrid/UniformGrid/MainWindow.xaml.cs
+++ UniformGrid/UniformGrid/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,7 +21,8 @@ namespace UniformGrid
     {
         
         #region Properties
-        public ObservableCollection<string> Days { get; set; } = new ObservableCollection<string>(new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" });
+        public ObservableCollection<string> DayOfWeek { get; set; } = new ObservableCollection<string>(new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" });
+        public ObservableCollection<string> Days { get; set; } = new ObservableCollection<string>();
         public string Month
         {
             get
@@ -34,24 +36,26 @@ namespace UniformGrid
         public MainWindow()
         {
             InitializeComponent();
-            InitGrid();
+            InitDays();
             DataContext = this;
         }
         #endregion
 
         #region Methods
-        private void InitGrid()
+        private void InitDays()
         {
-
             DateTime date = DateTime.Today;
             DateTime firstOfMonth = new DateTime(date.Year, date.Month, 1);
-
+            
             int dow = (int)firstOfMonth.DayOfWeek;
-            for (int i = 0; i < dow; i++)      
-                uniGrid.Children.Add(new Label());
+            Days = new ObservableCollection<string>();
+            for (int i = 0; i < dow; i++)
+                Days.Add("");
+            //uniGrid.Children.Add(new Label());
 
-            for (int i = 1; i <=  DateTime.DaysInMonth(date.Year, date.Month); i++)
-                uniGrid.Children.Add(new Label() { Content = i.ToString() });                
+            for (int i = 1; i <= DateTime.DaysInMonth(date.Year, date.Month); i++)
+                Days.Add(i.ToString());
+                //uniGrid.Children.Add(new Label() { Content = i.ToString() });                
         }
         #endregion
     }
