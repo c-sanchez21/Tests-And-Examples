cdadea33f56527c9ec060c0394d91a7c5fbeaa51
Cesar <c_sanchez21@hotmail.com>
2024-05-24 15:07:12 -0700

    Listview Test Added

---
+++ ListViewTest/ListViewTest/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListViewTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Properties
+        public ObservableCollection<Person> People { get; set; } = new ObservableCollection<Person>();
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            People.Add(new Person() { Name = "A", Age = 10 });
+            People.Add(new Person() { Name = "B", Age = 20 });
+            People.Add(new Person() { Name = "C", Age = 30 });
+        }
+    }
+}
---
+++ ListViewTest/ListViewTest/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewTest
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public Person() { }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+    }
+}
