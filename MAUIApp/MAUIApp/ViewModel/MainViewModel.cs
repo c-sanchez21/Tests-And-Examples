5ac4712704c10afa32b6601f8f3ff97217908c5c
Cesar <c_sanchez21@hotmail.com>
2024-06-25 19:18:54 -0700

    Updated MAUI Project

---
+++ MAUIApp/MAUIApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIApp.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        ///***Implemented by CommuntiyToolkit MVVM
+        //public event PropertyChangedEventHandler? PropertyChanged;
+        // protected void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        //  PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        [ObservableProperty]
+        string text;
+
+        [ICommand]
+        void Add()
+        {
+            Text = string.Empty;
+        }
+
+    }
+}
--- MAUIApp/MAUIApp/obj/Debug/net8.0-android/MAUIApp.AssemblyInfo.cs
+++ MAUIApp/MAUIApp/obj/Debug/net8.0-android/MAUIApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAUIApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+35620671f07832a6db93fc6d07842f950d8423eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3bb8875a431c6a75865a0b404254d6e36e0e034c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAUIApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAUIApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
