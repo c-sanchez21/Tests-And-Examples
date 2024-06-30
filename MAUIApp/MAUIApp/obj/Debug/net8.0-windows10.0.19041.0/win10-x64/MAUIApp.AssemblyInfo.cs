2e13349ca8dfe814352c00fb99608e747a945f83
Cesar <c_sanchez21@hotmail.com>
2024-06-29 22:58:56 -0700

    Slight changes to MAUI

--- MAUIApp/MAUIApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MAUIApp.AssemblyInfo.cs
+++ MAUIApp/MAUIApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MAUIApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MAUIApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e4af5c867e623bb1d3d366be225116e1f5e48e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4032e439131003008e55b1aa62e1a93b5e2b7be4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MAUIApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MAUIApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
